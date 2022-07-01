332d53f3b336b73f95e238e96342ac4bf24745ef
Emily1028 <81636593+Emily1028@users.noreply.github.com>
2022-07-01 22:27:21 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B0226497_段昱如_PDPR__Week14_1
+{
+    class Program
+    {
+        public static void Str(string a)
+        {
+            Console.WriteLine($"{a}");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("B0226497_段昱如_PDPR__Week14_1");
+            string s ="**********";
+            Str(s);
+            Console.ReadLine();
+
+        }
+    }
+}
